83dfafbbfece653bacab622f53488bb16bb458ba
saviganga <akatugbasaviour@gmail.com>
2021-07-27 07:51:26 -0700

    deploy

--- customuser/settings/prod.py
+++ customuser/settings/prod.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 DATABASES = {
     'default': {
